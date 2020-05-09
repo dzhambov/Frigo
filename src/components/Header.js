5f153c4ba6b64db2b82663133141587aea9e7bad
Hristo Dzhambov <dzhambov@hotmail.com>
2020-05-09 11:16:25 -0700

    start work on README.md

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Header(){
   return (
-    <h1>FRIGO</h1>
+    <div className="Header">
+      <h1>FRIGO</h1>
+      <h4>Your Personal Kitchen Assistant</h4>
+    </div>
   );
 }
 
