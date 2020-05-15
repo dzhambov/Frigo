25994ace1f488b81c75df4ab702497e48d8b2dbe
Hristo Dzhambov <dzhambov@hotmail.com>
2020-05-15 12:19:06 -0700

    add passing delete product test.

--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -12,7 +12,11 @@ export default (state = {}, action) => {
           id: id
         }
       });
-    default:
-    return state;
+      case 'DELETE_PRODUCT':
+        const newState = { ...state };
+        delete newState[id];
+        return newState;
+      default:
+      return state;
   }
 };
