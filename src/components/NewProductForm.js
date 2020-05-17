cc2559fd46d42312033244e51e213a612b6278b5
Hristo Dzhambov <dzhambov@hotmail.com>
2020-05-17 14:10:18 -0700

    add successfully updated new product form.

--- src/components/NewProductForm.js
+++ src/components/NewProductForm.js
@@ -1,30 +1,36 @@
 import React from "react";
-import { v4 } from "uuid";
+// import { v4 } from "uuid";
 import { PropTypes } from "prop-types";
 import ReusableForm from "./ReusableForm";
-import Moment from 'moment';
+// import Moment from 'moment';
+import { useFirestore } from 'react-redux-firebase';
 
 function NewProductForm(props) {
 
-  function handleNewProductFormSubmission(event) {
+  const firestore = useFirestore();
+
+  function addProductToFirestore(event) {
     event.preventDefault();
-    props.onNewProductCreation({
-      name: event.target.name.value,
-      brand: event.target.brand.value,
-      expiration: event.target.expiration.value,
-      price: event.target.price.value,
-      quantity: parseInt(event.target.quantity.value),
-      id: v4(),
-      timeBought: new Moment(),
-      formattedPassedTime: new Moment().fromNow(true)
-    });
+    props.onNewProductCreation();
+    return firestore.collection('product').add(
+      {
+        name: event.target.name.value,
+        brand: event.target.brand.value,
+        expiration: event.target.expiration.value,
+        price: event.target.price.value,
+        quantity: parseInt(event.target.quantity.value),
+        // id: v4(),
+        timeBought: firestore.FieldValue.serverTimestamp()
+        // formattedPassedTime: new Moment().fromNow(true)
+      }
+    );
   }
 
   return (
     <React.Fragment>
       <h3>Add New Product:</h3>
       <ReusableForm
-      formSubmissionHandler={handleNewProductFormSubmission}
+      formSubmissionHandler={addProductToFirestore}
       buttonText = "Add Product" />
     </React.Fragment>
   );
