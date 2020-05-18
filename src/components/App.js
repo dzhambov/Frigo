accafca1c85d7fb3991a2f1f4f189dee73896e30
Hristo Dzhambov <dzhambov@hotmail.com>
2020-05-17 17:08:19 -0700

    add authentication forms.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './Header';
 import ProductControl from './ProductControl';
-
+import Signin from './Signin';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
+    <Router>
       <Header />
-      <ProductControl />
-    </React.Fragment>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <ProductControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
-    <div className="Header">
-      <h1>FRIGO</h1>
-      <h4>Your Personal Kitchen Assistant</h4>
-    </div>
+    <React.Fragment>
+      <div className="Header">
+        <h1>FRIGO</h1>
+        <h4>Your Personal Kitchen Assistant</h4>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
   );
 }
 
