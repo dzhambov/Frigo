52fdfe48bb1acf1bc79a55017ca53fe652bae7b5
Hristo Dzhambov <dzhambov@hotmail.com>
2020-05-20 14:38:07 -0700

    add refactoring.

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,31 +24,17 @@ export const getRecipesFailure = (error) => ({
 });
 
 export const makeApiCall = () => {
-  return async dispatch => {
+  return dispatch => {
     dispatch(requestRecipes);
-    return await fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
+    return fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
       .then(response => response.json())
       .then(
         (jsonifiedResponse) => {
-          console.log(jsonifiedResponse);
           dispatch(getRecipesSuccess(jsonifiedResponse.hits));
-          console.log(jsonifiedResponse.hits[0].recipe.label);
+          console.log(jsonifiedResponse.hits);
         })
       .catch((error) => {
         dispatch(getRecipesFailure(error));
       });
   }
 }
-
-// export const useProduct = (product) => {
-//   const { name, brand, expiration, price, quantity, id } =product;
-//   return {
-//     type: c.USE_PRODUCT,
-//     name: name,
-//     brand: brand,
-//     expiration: expiration,
-//     price: price,
-//     quantity: quantity,
-//     id: id
-//   }
-// }
